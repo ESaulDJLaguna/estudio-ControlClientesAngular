8ed659f5cf77b8cc25ac4c68fe05c26e10d5ec21
ESaulDJLaguna <esdjl_asse15@hotmail.com>
2022-01-04 01:06:47 -0600

    Creación de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceroComponent } from './components/cabecero/cabecero.component';
+import { TableroComponent } from './components/tablero/tablero.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
+import { EditarClienteComponent } from './components/editar-cliente/editar-cliente.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { ConfigComponent } from './components/config/config.component';
+import { NoEncontradoComponent } from './components/no-encontrado/no-encontrado.component';
+import { PiePaginaComponent } from './components/pie-pagina/pie-pagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceroComponent,
+    TableroComponent,
+    ClientesComponent,
+    EditarClienteComponent,
+    AuthComponent,
+    RegistroComponent,
+    ConfigComponent,
+    NoEncontradoComponent,
+    PiePaginaComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
